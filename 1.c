3b7ea1c6bcc6566b825d89726a2cb3c08cef7bbd
Paradise_snow <101345246+n984261@users.noreply.github.com>
2022-03-21 20:00:25 +0800

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,146 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	add(); pdd();
+//	add(); pdd();
+//
+//
+//	return 0;
+//}
+//int add(void)
+//{
+//	printf("我");
+//}
+//int pdd(void)
+//{
+//	printf("NB\n");
+////}
+
+
+//#include <stdio.h>
+//int one(void);
+//int  two(void);
+//int main()
+//{
+//	printf("starting now\n");
+//	one();
+//	printf("Done");
+//	
+//
+//	return 0;
+//}
+//int one(void) {
+//	printf("one\n");
+//	two(); printf("\n");
+//	printf("three\n");
+//}
+//int two(void) {
+//	printf("two");
+//}
+
+//#define name;
+//#define surname;
+//#include <stdio.h>
+//int main()
+//
+//{                                   王卫写的错的
+//	printf("%s %s\n,name,surname");
+//	printf("%s,name");
+//	printf("%s,surname");
+//	printf("%s,%s,surname,name");
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	int a, b, c,d; float s;
+//	printf("输入");
+//	scanf_s("%d%d%d",&a,&b,&c);
+//	d = (a + b + c) / 2;
+//	s = sqrt
+//	(d * (d - a) * (d - b) * (d - c));
+//	printf("%f", s);
+//
+//	return 0;
+//}
+
+//
+//#include <stdio.h>
+//int main()
+//{
+//	float b, c; int a1,a2;
+//	b = 35.425;
+//	c = 52.954;
+//	a1 = b + c;
+//	printf("a1=%d\n", a1);
+//
+//	a2= (int)b +(int)c;
+//	printf("a2=%d", a2);
+//
+//	return 0;
+//}
+//
+
+//#include <stdio.h>
+//int main()
+//{
+//	int  a, b, c,d;
+//	a = 100, b = 50;
+//	if (a > b)
+//	{
+//		c = a;
+//		d = b * b;
+//	}
+//	else
+//	{
+//		c = b;
+//		d = a;
+//
+//	}
+//	printf("c=%dd=%d", c, d);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b, c, d;
+//	a = 100, b = 50;
+//	(a > b) ? (c = a) : (c = b);
+//	(a < b) ? (d = a) : (d = b * b);
+//	printf("c=%dd=%d", c, d);
+//
+//
+//	return 0;
+//}
+//
+
+
+
+
+
+
+
+
+
+
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	char a, b, c; float d;
+//	printf("请输入abc\n");
+//	scanf_s("%c%c%c", &a,1, &b,1, &c,1);
+//	d = (a + b + c) / 3;
+//	printf("%f", d);
+//
+//
+//	return 0;
+//}
