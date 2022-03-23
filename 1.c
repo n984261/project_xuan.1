d652a675f68413a8cb50778c6d36255dc6f38c8d
Paradise_snow <101345246+n984261@users.noreply.github.com>
2022-03-23 19:21:03 +0800

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,146 +1,46 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
+//#include <stdio.h>
 //
-//int main(void)
+//
+//  int add( int sum3, int sum4)
+//         {
+//		int z = 0;
+//		z = sum3 + sum4;
+//
+//		return z;
+//      }
+//	int main()
 //{
-//	add(); pdd();
-//	add(); pdd();
+//		int sum1, sum2; float sum3;
 //
-//
+//	scanf_s("%d%d", &sum1,& sum2);
+//	//int sum = sum1 + sum2;
+//	int sum = add(sum1, sum2);
+//	printf("sum=%d",sum);
 //	return 0;
 //}
-//int add(void)
-//{
-//	printf("我");
-//}
-//int pdd(void)
-//{
-//	printf("NB\n");
-////}
 
 
 //#include <stdio.h>
-//int one(void);
-//int  two(void);
 //int main()
 //{
-//	printf("starting now\n");
-//	one();
-//	printf("Done");
+//	int a[10] = { 1,2,3,4,5,6,7,8,9 };
+//	int b = 0;
+//	while (b < 10)
+//	{
+//		printf("%d ", a[b]);
+//		b++;
+//	}
 //	
-//
-//	return 0;
-//}
-//int one(void) {
-//	printf("one\n");
-//	two(); printf("\n");
-//	printf("three\n");
-//}
-//int two(void) {
-//	printf("two");
-//}
-
-//#define name;
-//#define surname;
-//#include <stdio.h>
-//int main()
-//
-//{                                   王卫写的错的
-//	printf("%s %s\n,name,surname");
-//	printf("%s,name");
-//	printf("%s,surname");
-//	printf("%s,%s,surname,name");
-//
-//	return 0;
-//}
-
-//#include <stdio.h>
-//#include <math.h>
-//int main()
-//{
-//	int a, b, c,d; float s;
-//	printf("输入");
-//	scanf_s("%d%d%d",&a,&b,&c);
-//	d = (a + b + c) / 2;
-//	s = sqrt
-//	(d * (d - a) * (d - b) * (d - c));
-//	printf("%f", s);
-//
-//	return 0;
-//}
-
-//
-//#include <stdio.h>
-//int main()
-//{
-//	float b, c; int a1,a2;
-//	b = 35.425;
-//	c = 52.954;
-//	a1 = b + c;
-//	printf("a1=%d\n", a1);
-//
-//	a2= (int)b +(int)c;
-//	printf("a2=%d", a2);
-//
-//	return 0;
-//}
-//
-
-//#include <stdio.h>
-//int main()
-//{
-//	int  a, b, c,d;
-//	a = 100, b = 50;
-//	if (a > b)
-//	{
-//		c = a;
-//		d = b * b;
-//	}
-//	else
-//	{
-//		c = b;
-//		d = a;
-//
-//	}
-//	printf("c=%dd=%d", c, d);
-//
 //	return 0;
 //}
 
 //#include <stdio.h>
 //int main()
 //{
-//	int a, b, c, d;
-//	a = 100, b = 50;
-//	(a > b) ? (c = a) : (c = b);
-//	(a < b) ? (d = a) : (d = b * b);
-//	printf("c=%dd=%d", c, d);
-//
-//
+//	//   1.float a = 9 / (float)2;
+//	//2.
+//	float a = 9 / 2.0;
+//		printf("%f ", a);
 //	return 0;
 //}
-//
 
-
-
-
-
-
-
-
-
-
-
-
-//#include <stdio.h>
-//int main()
-//{
-//	char a, b, c; float d;
-//	printf("请输入abc\n");
-//	scanf_s("%c%c%c", &a,1, &b,1, &c,1);
-//	d = (a + b + c) / 3;
-//	printf("%f", d);
-//
-//
-//	return 0;
-//}
