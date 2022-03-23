0a0c1a998b383f5b3bf745ff0e4de8434b253a65
Paradise_snow <101345246+n984261@users.noreply.github.com>
2022-03-23 19:23:40 +0800

    第二次

---
+++ 2.c
@@ -0,0 +1,46 @@
+//#include <stdio.h>
+//
+//
+//  int add( int sum3, int sum4)
+//         {
+//		int z = 0;
+//		z = sum3 + sum4;
+//
+//		return z;
+//      }
+//	int main()
+//{
+//		int sum1, sum2; float sum3;
+//
+//	scanf_s("%d%d", &sum1,& sum2);
+//	//int sum = sum1 + sum2;
+//	int sum = add(sum1, sum2);
+//	printf("sum=%d",sum);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a[10] = { 1,2,3,4,5,6,7,8,9 };
+//	int b = 0;
+//	while (b < 10)
+//	{
+//		printf("%d ", a[b]);
+//		b++;
+//	}
+//	
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	//   1.float a = 9 / (float)2;
+//	//2.
+//	float a = 9 / 2.0;
+//		printf("%f ", a);
+//	return 0;
+//}
+
