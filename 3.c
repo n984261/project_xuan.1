e1e87ab2bba19257735a074ad9987beb9002f3e2
Paradise_snow <101345246+n984261@users.noreply.github.com>
2022-03-28 18:38:33 +0800

    Add files via upload
    if else , switch

---
+++ 3.c
@@ -0,0 +1,49 @@
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	float a,y;
+//	printf("please input\n");
+//	scanf_s("%f", &a);
+//	if (a <= 10.0)
+//		y = sin(a);
+//	else if (a <= 15.0)
+//		y = 2.0 * exp(a - 10.0);
+//	else if (a > 15.0)
+//		y = log(a - 2.0);
+//	printf("a=%07.2f  y=%07.2f", a, y);
+//
+//
+//
+//
+//	return 0;
+//}
+//
+
+//#include <stdio.h>
+//int main()
+//{
+//	int score, y;
+//	printf("score=");
+//	 scanf_s("%d", &score);
+//	 y = (score / 10);
+//	 if (score < 0 || score>100)
+//		 printf("error");
+//	 switch(y)
+//	 {
+//	 case 1:
+//	 case 2:
+//	 case 3:
+//	 case 4:
+//	 case 5:printf("E");
+//	 case 6:printf("D");
+//	 case 7:printf("C");
+//	 case 8:printf("B");
+//	 default:printf("A");
+//
+//	 }
+//
+//
+//	return 0;
+//}
+
